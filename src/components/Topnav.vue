7f45faa83cc3a0ccfd506a5200429966211afc88
taiy <1726311422@qq.com>
2022-07-10 21:13:39 +0800

    完善 aside 切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,15 +1,19 @@
 <template>
     <div class="topnav">
-      <div class="logo" @click="toggleAside">LOGO</div>
+      <div class="logo">LOGO</div>
       <ul class="menu">
         <li>菜单1</li>
         <li>菜单2</li>
       </ul>
+      <span class="toggleAside" @click="toggleAside">
+
+      </span>
     </div>
 </template>
 
 <script lang="ts">
 import {inject, Ref} from 'vue';
+
 export default {
   setup(){
     const asideVisible=inject<Ref<boolean>>('asideVisible')
@@ -28,6 +32,8 @@ export default {
   padding: 16px;
   position:relative;
   z-index: 10;
+  justify-content: center;
+  align-items: center;
   > .logo {
     max-width: 6em;
     margin-right: auto;
@@ -38,6 +44,27 @@ export default {
     flex-wrap: nowrap;
     > li {
       margin: 0 1em;
+    }
+  }
+  > .toggleAside{
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    display: none;
+  }
+  @media (max-width: 500px){
+    > .menu{
+      display: none;
+    }
+    > .logo{
+      margin:0 auto;
+    }
+    >.toggleAside{
+      display: inline-block;
     }
   }
 }
