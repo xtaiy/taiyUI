a06a23c3aa5f75b1f13b6cd23f241aee5cb7eb09
taiy <1726311422@qq.com>
2022-07-10 21:54:40 +0800

    基本完成官网框架

--- src/router.ts
+++ src/router.ts
@@ -3,17 +3,25 @@ import {createRouter, createWebHashHistory} from "vue-router";
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
 import Switch from './components/Switch.vue'
-const routes = [
-    { path: '/', component: Home },
-    { path: '/doc', component: Doc ,
-        children:[{
-            path:'switch',component:Switch}
-        ]},
-]
+import Button from './components/Button.vue'
+import Dialog from './components/Dialog.vue'
+import Tabs from './components/Tabs.vue'
+
+const history = createWebHashHistory();
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes,
+    history,
+    routes:[
+        { path: '/', component: Home },
+        { path: '/doc', component: Doc ,
+            children:[
+                {path:'switch',component:Switch},
+                {path:'button',component:Button},
+                {path:'dialog',component:Dialog},
+                {path:'tabs',component:Tabs}
+            ]
+        },
+    ],
 })
 
 export {router}
