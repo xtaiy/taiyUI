28f32ecf64f5a844b42fc33d5f6b952650c24b50
taiy <1726311422@qq.com>
2022-07-11 19:56:48 +0800

    添加 Button 组件的 level 属性以及 CSS

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -16,14 +16,19 @@ export default {
    size:{
      type:String,
      default: 'normal'
-   }
+   },
+   level: {
+     type: String,
+     default: "normal",
+   },
  },
   setup(props){
-   const {theme,size}=props
+   const {theme,size,level}=props
    const classes=computed(()=>{
      return {
        [`taiy-theme-${theme}`]:theme,
-       [`taiy-size-${size}`]:size
+       [`taiy-size-${size}`]:size,
+       [`taiy-level-${level}`]: level,
      }
    })
     return {classes}
@@ -37,6 +42,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .taiy-button {
   box-sizing: border-box;
   height: $h;
@@ -51,6 +57,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -90,6 +97,54 @@ $radius: 4px;
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+
+  &.taiy-theme-button {
+    &.taiy-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.taiy-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.taiy-theme-link {
+    &.taiy-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.taiy-theme-text {
+    &.taiy-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.taiy-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 </style>
