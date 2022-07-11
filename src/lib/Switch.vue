3bcb863b5b1231c7c76884861d3e7a99f1a53ce2
taiy <1726311422@qq.com>
2022-07-11 10:34:21 +0800

    给 Switch 组件添加了动画

---
+++ src/lib/Switch.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <button :class="{checked}" @click="toggle"> <span></span> </button>
+  </div>
+</template>
+
+<script lang="ts">
+  import {ref} from 'vue';
+  import ButtonDemo from '../components/ButtonDemo.vue';
+
+  export default {
+    components: {ButtonDemo},
+    setup(){
+      const checked=ref(false)
+      const toggle=()=>{
+        checked.value =! checked.value;
+      }
+      return {checked,toggle}
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  $h:22px;
+  $h2:$h - 4px;
+  button{
+    height: $h;
+    width: $h * 2;
+    border:none;
+    background: gray;
+    border-radius: $h/2;
+    position: relative;
+  }
+  span{
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h/2;
+    transition: left 250ms;
+  }
+  button.checked{
+    background: blue;
+  }
+  button.checked >span{
+    left: calc(100% - #{$h2} - 2px);
+  }
+</style>
