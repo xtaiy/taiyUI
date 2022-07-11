43bc70c5ff418c1fb016033e12e7f08824d7714b
taiy <1726311422@qq.com>
2022-07-11 11:11:37 +0800

    给 Switch 组件添加了 value 属性

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -14,7 +14,7 @@
     components: {ButtonDemo},
     setup(props,context){
       const toggle=()=>{
-         context.emit('input',!props.value)
+         context.emit('update:value',!props.value)
       }
       return {toggle}
     }
