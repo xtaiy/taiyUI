b795d8651f2fadb138f8352f72c2c961259f3159
taiy <1726311422@qq.com>
2022-07-12 19:42:39 +0800

    Dialog 组件,一句话打开Dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <h1> 示例1 </h1>
     <Button @click="toggle">toggle</Button>
     <Dialog v-model:visible="visible" :closeOnClickOverlay="false" :ok="f1" :cancel="f2">
       <template v-slot:content>
@@ -9,13 +9,15 @@
         <strong>加粗的标题</strong>
       </template>
     </Dialog>
-  </div>
+  <h1> 示例2 </h1>
+  <Button @click="showDialog">show</Button>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue';
 import Button from '../lib/Button.vue';
 import {ref} from 'vue';
+import {openDialog} from '../lib/openDialog';
 
 export default {
   components:{
@@ -32,7 +34,13 @@ export default {
     }
     const f2=()=>{
     }
-    return {visible,toggle,f1,f2}
+    const showDialog=()=>{
+      openDialog({title:'标题',content:'你好',
+        ok:()=>{return true},
+        cancel:()=>{return false}
+      })
+    }
+    return {visible,toggle,f1,f2,showDialog}
   }
 }
 </script>
