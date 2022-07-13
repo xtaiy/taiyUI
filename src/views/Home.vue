86831c9e32c9f00fc7bb0f87cd25dfab6fcfc06d
taiy <1726311422@qq.com>
2022-07-13 21:10:09 +0800

    从 iconfont 引入图标

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,29 @@
 <template>
-  <div class="topnavAndBanner">
-    <Topnav/>
-    <div class="banner">
-      <h1>taiyUI</h1>
-      <h2>一个很不错的 UI</h2>
-      <p class="actions">
-        <a href="https://github.com">Github</a>
-        <router-link to="/doc">开始</router-link>
-      </p>
+  <div>
+    <div class="topnavAndBanner">
+      <Topnav/>
+      <div class="banner">
+        <h1>taiyUI</h1>
+        <h2>一个很不错的 UI</h2>
+        <p class="actions">
+          <a href="https://github.com">Github</a>
+          <router-link to="/doc">开始</router-link>
+        </p>
+      </div>
+    </div>
+    <div class="features">
+      <svg class="icon" >
+        <use xlink:href="#icon-Vue"></use>
+      </svg>
+      <svg class="icon" >
+        <use xlink:href="#icon-ts"></use>
+      </svg>
+      <svg class="icon" >
+        <use xlink:href="#icon-light"></use>
+      </svg>
     </div>
   </div>
+
 </template>
 
 <script lang="ts">
@@ -20,10 +34,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$green:#02bcb0;
+$border-radius:4px;
+$color: #007974;
 .topnavAndBanner{
   background: linear-gradient(135deg, rgba(227,255,253,1) 0%, rgba(183,233,230,1) 100%);
 }
+.features{
+  >svg{
+    width: 64px;
+    height: 64px;
+  }
+}
 .banner {
+  color: $color;
   padding: 100px 0;
   display: flex;
   justify-content: center;
@@ -33,13 +57,14 @@ export default {
     padding: 8px 0;
     a {
       margin:0 8px;
-      background: #fff;
+      background: $green;
+      color:white;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      padding: 8px 24px;
+      border-radius: $border-radius;
+      &:hover{
+        text-decoration: none;
+      }
     }
   }
 }
