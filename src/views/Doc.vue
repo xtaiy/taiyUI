541a901899c6e3374fa9a47ec4e05170bbe68a68
taiy <1726311422@qq.com>
2022-07-14 21:15:27 +0800

    复杂一点的高亮

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -54,9 +54,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.router-link-active{
-  text-decoration: underline;
-}
+
 .layout {
   display: flex;
   flex-direction: column;
@@ -90,16 +88,25 @@ aside {
   position: fixed;
   top: 0;
   left: 0;
-  padding: 70px 16px 16px;
+  padding: 70px 0 16px;
   height: 100%;
   z-index: 10;
   > h2 {
     margin-bottom: 4px;
+    padding: 0 16px;
   }
 
   > ol {
     > li {
-      padding: 4px 0;
+      a{
+        display: block;
+        padding: 4px 16px;
+        text-decoration: none;
+      }
+      .router-link-active{
+        background: white;
+      }
+
     }
   }
 }
