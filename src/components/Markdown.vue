429486d56a3e8b884b52a2c22dc7c868dbb4cb0a
taiy <1726311422@qq.com>
2022-07-16 17:43:42 +0800

    部分代码移植

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -1,24 +1,27 @@
 <template>
   <article class="markdown-body" v-html="content">
-
   </article>
 </template>
 
 <script lang="ts">
-import {ref} from 'vue';
+import {
+  ref
+} from 'vue'
 export default {
-  props:{
-    path:{
-      type:String,
-      required:true
+  props: {
+    path: {
+      type: String,
+      required: true
     }
   },
   setup(props) {
-    const content=ref<string>('')
-    import(props.path).then(result=>{
-      content.value=result.default
+    const content = ref < string > ('')
+    import(props.path).then(result => {
+      content.value = result.default
     })
-    return {content}
+    return {
+      content
+    }
   }
 }
 </script>
--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -22,7 +22,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{Switch2Demo.__sourceCode}}</pre>
+        <pre class="language-html" v-html="Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html, 'html')" />
       </div>
     </div>
   </div>
@@ -34,19 +34,15 @@ import Button from '../lib/Button.vue'
 import {ref} from 'vue';
 import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
-import "../prismjs/prism.js";
-// 引用css
-import "../prismjs/themes/prism-okaidia.css";
-//prismjs在window对象下有Prism属性
-// 使用any类型骗过ts检测
-const P = (window as any).Prism;
-
+import 'prismjs'
+import 'prismjs/themes/prism.css'
+const Prism = (window as any).Prism
 
 export default {
   components: {Button},
   setup(){
     const bool=ref(true)
-    return {bool,Switch1Demo,Switch2Demo,P}
+    return {bool,Switch1Demo,Switch2Demo,Prism}
   }
 }
 </script>
