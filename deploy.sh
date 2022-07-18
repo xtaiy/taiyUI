a50af0e5e488bffcb159e5202475c4a111468c06
taiy <1726311422@qq.com>
2022-07-18 18:42:21 +0800

    创建 lib/index.ts

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 rm -rf dist &&
 yarn build &&
-cd dist &&
+cd dist
 git init &&
 git add . &&
-git commit -m "更新" &&
+git commit -m "first commit" &&
 git branch -M main &&
-git remote add origin git@github .com:xtaiy/taiyUI-website.git &&
+git remote add origin git@github.com:xtaiy/taiyUI-website.git &&
 git push -f -u origin main &&
 cd ..
 echo https://xtaiy.github.io/taiyUI-website/index.html
