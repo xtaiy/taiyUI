265a29adf8c78ae60c0f6b035d521a5aedf90845
taiy <1726311422@qq.com>
2022-07-18 17:33:58 +0800

    完成 demo 组件

--- src/router.ts
+++ src/router.ts
@@ -5,11 +5,17 @@ import SwitchDemo from "./components/SwitchDemo.vue";
 import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
+// @ts-ignore
+import intro from './markdown/intro.md'
+// @ts-ignore
+import getStart from './markdown/get-start.md'
+// @ts-ignore
+import install from './markdown/install.md'
 
 import { h } from 'vue';
 import Markdown from './components/Markdown.vue';
 const history = createWebHashHistory();
-const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
+const md = string => h(Markdown, { content: string, key: string })
 export const router = createRouter({
   history: history,
   routes: [
@@ -19,9 +25,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: "", redirect: '/doc/intro' },
-        { path: "intro", component: md('intro') },
-        { path: "get-started", component: md('get-start') },
-        { path: "install", component: md('install') },
+        { path: "intro", component: md(intro) },
+        { path: "get-started", component: md(getStart) },
+        { path: "install", component: md(install) },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
         { path: "dialog", component: DialogDemo },
